a483ca0f775f53bfb8b70827701ecbebc73fd5a2
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-19 15:06:55 +0200

    roles

--- main.js
+++ main.js
@@ -29,15 +29,20 @@ client.on('message', message => {
                 message.channel.send('you dont have permissions to do that')
             }
 
-             if(command == 'AddAdmin'){
-                message.channel.send('you are Admin now :)')
-                message.member.roles.add('778962085366333480');
-            }
-            else if (message == 'RemoveAdmin'){
-                message.channel.send('you are not admin anymore :(')
-                message.member.roles.remove('778962085366333480');
-            }
+            
         
+    }
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'AddAdmin'){
+        message.channel.send('you are Admin now! :)');
+        message.member.roles.add('778962085366333480');
     }
 })
 
