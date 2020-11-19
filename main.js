06fa2cb6bb6c7d96d47945ee2a29c533ba5fded8
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-20 00:45:15 +0200

    update

--- main.js
+++ main.js
@@ -20,11 +20,11 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'hi'){
-        message.channel.send('hi!');
-    } else if(command == 'admin'){
+    if (command === 'admin'){
         message.member.roles.add('778962085366333480');
         message.channel.send('you are Admin now! :)');
+    
+    } else if(command == 'dmin'){
     }
 })
 
