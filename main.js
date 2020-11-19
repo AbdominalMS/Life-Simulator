7d3deb8c3e30301c24a9c714c3f9baca89c1ca56
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-20 01:24:57 +0200

    update

--- main.js
+++ main.js
@@ -21,15 +21,19 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
 
     if (command === 'admin'){
+        if(message.member.roles.cache.has('779120873466101830')){
         message.member.roles.add('778962085366333480');
         message.channel.send('you are Admin now! :)');
-    
-    } else if(command == 'admin-'){
+        } if(message.member.roles.cache.has('778962085366333480')){
+            message.channel.send('you already have an Admin');
+        }
+     } else if(command == 'admin-'){
+         if(message.member.roles.cache.has('778962085366333480')){
         message.member.roles.remove('778962085366333480');
-        message.channel.send('you r not Admin anymore!');
-    }
+        message.channel.send('you are not Admin anymore!');
+        if (message.member.roles.cache.has('779120873466101830'))
+           message.channel.send('you are already not an Admin!');
+         }
+        }
 })
-
-
-
 client.login(process.env.token);
