e0172866064321d517b45653d6ec65ef0a6367f1
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-20 15:23:25 +0200

    fix

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ client.on('message', message => {
     if(!args[0]) return message.reply("please enter the amount of messages that you want to clear!");
     if(isNaN(args[0])) return message.reply("please enter a real number!");
     if(args[0] > 100) return message.reply("you can not delete more than 100 message!");
-    if(args[0 < 1]) return message.reply("you must delete atleast one message!");
+    if(args[0] < 1) return message.reply("you must delete atleast one message!");
     if (command === 'admin'){
         if(message.member.roles.cache.has('779120873466101830')){
         message.member.roles.add('778962085366333480');
