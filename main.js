a18a90d8975a2ad5e0a38d4171d5406bf1753aad
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-20 11:18:29 +0200

    update

--- main.js
+++ main.js
@@ -34,8 +34,8 @@ client.on('message', message => {
         if (message.member.roles.cache.has('779120873466101830'))
            message.channel.send('you are already not an Admin!');
          }
-        } else if(command == 'hi'){
-            message.channel.send('hi!');
+        }   else if(command == 'hi'){
+            message.channel.send('hi!'); 
         }
 })
 client.login(process.env.token);
