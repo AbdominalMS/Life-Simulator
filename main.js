579d133330113f179efa825e7d3e0db40c5925cf
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-22 19:15:31 +0200

    fix

--- main.js
+++ main.js
@@ -62,43 +62,7 @@ client.on('message', async message => {
             msgEmbed.react('👧');
         } else if (command == 'rps'){
             if (message.channel.id === "779994077436706826"){
-               const chooseArr = ["🗻", "📰", "✂"];
-          
-        const embed = new MessageEmbed()
-            .setColor("#ffffff")
-            .setFooter(message.guild.me.displayName, client.user.displayAvatarURL)
-            .setDescription("Add a reaction to one of these emojis to play the game!")
-            .setTimestamp();
-
-        const m = await message.channel.send(embed);
-        // Wait for a reaction to be added
-        const reacted = await promptMessage(m, message.author, 30, chooseArr);
-
-        // Get a random emoji from the array
-        const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
-
-        // Check if it's a win/tie/loss
-        const result = await getResult(reacted, botChoice);
-        // Clear the reactions
-        await m.reactions.removeAll()
-
-        embed
-            .setDescription("")
-            .addField(result, `${reacted} vs ${botChoice}`);
-
-        m.edit(embed);
-
-        function getResult(me, clientChosen) {
-            if ((me === "🗻" && clientChosen === "✂") ||
-                (me === "📰" && clientChosen === "🗻") ||
-                (me === "✂" && clientChosen === "📰")) {
-                    return "You won!";
-            } else if (me === clientChosen) {
-                return "It's a tie!";
-            } else {
-                return "You lost!";
-            }
-        }
+              
             }
         }
        
