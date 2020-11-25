080ffc0fbb793a8ecebedca4f0b230961ec487db
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-26 00:32:13 +0200

    kick added!!

--- main.js
+++ main.js
@@ -76,40 +76,23 @@ let comp_val = rps[parseInt(comp_res)];
     return message.channel.send(`You chose **${args[0]}** and I chose **${comp_val}** and I lost! Congrats on winning!`);
   }
 }
-        } else if (command == 'ban'){
-           if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
-
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-
-        if(!args[0]) return message.channel.send('Please specify a user');
-
-        if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
-        if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
-
-        if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
-
-        let reason = args.slice(1).join(" ");
-
-        if(!reason) reason = 'Unspecified';
-
-        member.ban(`${reason}`).catch(err => { 
-          message.channel.send('Something went wrong')
-            console.log(err)
-        })
-
-        const banembed = new Discord.MessageEmbed()
-        .setTitle('Member Banned')
-        .setThumbnail(member.user.displayAvatarURL())
-        .addField('User Banned', member)
-        .addField('Kicked by', message.author)
-        .addField('Reason', reason)
-        .setFooter('Time kicked', client.user.displayAvatarURL())
-        .setTimestamp()
-
-        message.channel.send(banembed);
-
-
+        } else if (command == 'kick'){
+           if(!message.member.hasPermission("KICK_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('you dont have permissions')
+            let user = message.mentions.users.first();
+            
+            let member = message.guild.member(user);
+            let reason = args.slice(1).join(" ");
+            
+            if(!user) return message.channel.send("please mention the user");
+            if(user.id === message.author.id) return message.channel.send("you cant kick yourself");
+            if(user.id === client.user.id) return message.channel.send("you can't kick me")
+            if(!reason) reason = "No reason specified"
+            
+            member.kick(reason).then(() => {
+                message.channel.send(`successfuly kicked ${user.tag}`);
+            }).catch(err => {
+                message.reply("I was unable to kick the member.");
+            })
     }
 }
             
