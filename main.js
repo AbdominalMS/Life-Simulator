8a67e7e075ec6d6f932ec387fa543a955a49fe23
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2020-11-26 00:15:01 +0200

    ban fixed!!

--- main.js
+++ main.js
@@ -77,23 +77,41 @@ let comp_val = rps[parseInt(comp_res)];
   }
 }
         } else if (command == 'ban'){
-            const { member, mentions } = postMessage
-            const tag = `<@${member.id}>`
-            
-            if (message.member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')){
-                const target = mentions.users.first()
-                if (target){
-                    const targetMember = message.guild.members.cache.get(target.id)
-                    targetMember.ban()
-                    message.channel.send(`${tag}`)
-                }
-                else {
-                    message.channel.send(`${tag}please specify someone to ban`)
-                }
-            }
-            else {
-                message.channel.send(`${tag}you do not have permission to use this command`)
-            }
+           if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
+
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+        if(!args[0]) return message.channel.send('Please specify a user');
+
+        if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
+        if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
+
+        if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
+
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = 'Unspecified';
+
+        guildMember.ban({ days: 7, reason: 'They deserved it' })
+  .then(console.log)
+  .catch(console.error);
+        })
+
+        const banembed = new Discord.MessageEmbed()
+        .setTitle('Member Banned')
+        .setThumbnail(member.user.displayAvatarURL())
+        .addField('User Banned', member)
+        .addField('Kicked by', message.author)
+        .addField('Reason', reason)
+        .setFooter('Time kicked', client.user.displayAvatarURL())
+        .setTimestamp()
+
+        message.channel.send(banembed);
+
+
+    }
+}
             
         }  else if (command == 'clear'){ 
             if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_ROLES ')){
