894cb820adce3c35809763187bf60b13b512f45e
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2021-01-05 22:11:04 +0200

    fix

--- main.js
+++ main.js
@@ -181,11 +181,12 @@ let comp_val = rps[parseInt(comp_res)];
                     
                   message.channel.send('starting after 5 sec..')
                     setTimeout(() => {
-                        message.channel.send('choose a number between 1 to 10')  
-                    },5000);
-                     if (command == 1){
+                        message.channel.send('choose a number between 1 to 10')
+                        if (message.content == 1){
                              message.channel.send('working')
                              }
+                    },5000);
+                     
                    
                     
                    
