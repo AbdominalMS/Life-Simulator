1b53156fd698a42dab81d38f47a0ad3ac6c69905
AbdominalMS <74714337+AbdominalMS@users.noreply.github.com>
2021-01-07 15:06:33 +0200

    fix

--- main.js
+++ main.js
@@ -119,9 +119,13 @@ let comp_val = rps[parseInt(comp_res)];
             if(isNaN(args[0])) return message.reply("please Enter a real number")
             if (args[0] > 100) return message.reply("you can't delete more than 100 messages!")
             if (args[0] < 1)   return message.reply("you must delete atleast one message!")
+                const deleted = args[0];
             await message.channel.messages.fetch({limit: args[0]}).then(messages => {
                 message.channel.bulkDelete(messages);
             })
+                setTimeout(() =>{
+                    message.channel.send(`${deleted}Messages Deleted`)
+                },1000)
             }
         } 
           else if (command == 'love'){
