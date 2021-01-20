c59c144068fde6cee7f9d678dbf1b211734b2474
Abdelrahman Moustafa <74714337+AbdominalMS@users.noreply.github.com>
2021-01-20 22:22:31 +0200

    Embed for the Hug command

--- main.js
+++ main.js
@@ -243,12 +243,13 @@ let comp_val = rps[parseInt(comp_res)];
               
                 }
                 else if (command == 'hug'){
-                    if (message.mentions.members.first() == message.author.id){
-                        message.channel.send(`${message.author} sent a hug to ${message.mentions.memebers.first()} https://tenor.com/view/sailor-moon-black-lady-hug-self-anime-gif-15558731`)
-                    }
-                    else {
-                    message.channel.send(`${message.author} sent a hug to ${message.mentions.members.first()} https://tenor.com/view/noragami-kofuku-daikoku-hugging-love-gif-14637016`);
-                    }
+                   const newEmbed = new Discord.MessageEmbed()
+                   .setColor('#304281')
+                   .setTitle(`${message.author} sent a hug to ${message.mentions.members.first()}`)
+                   .addFields(
+                       {name: 'Hug' , value: 'https://tenor.com/view/noragami-kofuku-daikoku-hugging-love-gif-14637016'}
+                   )
+                    message.channel.send(newEmbed);
 
                 }
     
