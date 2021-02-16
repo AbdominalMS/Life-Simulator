440d9daed6b6d18dc013edaeacc975ff0da4f85f
Abdelrahman Moustafa <74714337+AbdominalMS@users.noreply.github.com>
2021-02-16 08:32:51 -0800

    Slap

--- main.js
+++ main.js
@@ -265,7 +265,7 @@ let comp_val = rps[parseInt(comp_res)];
                     message.channel.send(`${message.author} Sent you friend request do you accept? ${message.mentions.members.first()}`)
                 }
             } else if (command == 'slap'){
-                const slap = ['https://tenor.com/view/bunny-girl-slap-angry-girlfriend-anime-gif-15144612','https://tenor.com/view/girl-slap-anime-mad-student-gif-17423278','https://tenor.com/view/no-angry-anime-slap-gif-7355956','https://tenor.com/view/anime-slap-slap-in-the-face-smash-gif-17314633','https://tenor.com/view/anime-manga-japanese-anime-japanese-manga-toradora-gif-5373994']
+            const slap = ['https://tenor.com/view/bunny-girl-slap-angry-girlfriend-anime-gif-15144612','https://tenor.com/view/girl-slap-anime-mad-student-gif-17423278','https://tenor.com/view/no-angry-anime-slap-gif-7355956','https://tenor.com/view/anime-slap-slap-in-the-face-smash-gif-17314633','https://tenor.com/view/anime-manga-japanese-anime-japanese-manga-toradora-gif-5373994','https://tenor.com/view/anime-slap-hit-hurt-angry-gif-12396060'];
                 const random = Math.floor(Math.random() * slap.length);
                 message.channel.send(`${message.author} Slapped you!! ${message.mentions.members.first()}${slap.random}`)
             }   
